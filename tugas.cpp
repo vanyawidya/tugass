6dca6a01d57f28b4fcec4bef79aa04be28b20558
vanyawidya <56577127+vanyawidya@users.noreply.github.com>
2021-03-02 10:52:57 +0700

    Add files via upload

---
+++ tugas.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	int x,y,a,b,c,d;
+	x=40,y=5;
+
+	cout<<"Masukkan Nilai x : "<<x<<endl;
+	cout<<"Masukkan Nilai y : "<<y<<endl;
+
+	a=x+y;
+	b=x-y;
+	c=x*y;
+	d=x/y;
+
+	cout<<"Hasil Penjumlahan dari X dan Y adalah : "<<a<<endl;
+	cout<<"Hasil Pengurangan dari X dan Y adalah : "<<b<<endl;
+	cout<<"Hasil Perkalian dari X dan Y adalah : "<<c<<endl;
+	cout<<"Hasil Pembagian dari X dan Y adalah : "<<d<<endl;
+	return 0;
+
+
+}
